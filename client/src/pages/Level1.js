a63a59ee453efa3ab8bf18238dab23663815194d
Lilly <lilly@DESKTOP-I6QR8AT.(none)>
2022-05-12 18:11:52 +0200

    logic level 1

--- client/src/pages/Level1.js
+++ client/src/pages/Level1.js
@@ -1,15 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import allTask from "../level1.json";
+import "../style/Level1.css";
+import axios from 'axios'
+import { useParams, useNavigate } from 'react-router-dom'
+
+
 
 function Level1() {
   const [task, setTask] = useState(allTask);
+  const [countTask, setTaskCount] = useState(0);
+  const [points, setScore] = useState(0); 
+  const [infoBox, setInfoBox] = useState("");
+  const [feedback, setFeedback] = useState("");
 
-  console.log(task);
+
+/*   useEffect(() => {
+		console.log('use effect')
+		axios.get(`/api/auth/')
+			.then(response => {
+				const { points} = response.data
+				setScore(points)
+			})
+			.catch(err => console.log(err))
+	}, [])
+ */
+
+const clickYes = () => { 
+  setTaskCount(countTask +1)
+  setInfoBox("")
+
+  if(task[countTask].answer){
+    setScore(points +1)
+    setFeedback(task[countTask].infoIfCorrect)
+    console.log("correct")
+  } else {
+    setFeedback(task[countTask].infoIfWrong)
+    console.log("wrong")
+  }
+
+}
+
+const clickNo = () => { 
+  setTaskCount(countTask +1)
+  setInfoBox("")
+  if(!task[countTask].answer){
+    console.log("correct")
+    setScore(points +1)
+    setFeedback(task[countTask].infoIfCorrect)
+  } else {
+    console.log("wrong")
+    setFeedback(task[countTask].infoIfWrong)
+  }
+ }
+
+const clickInfo = () => { setInfoBox(task[countTask].infoText) }
+
+
   return (
     <div>
-      <div>
-        
-        <h2>{task[0].text}</h2>
+      <div className="container-level1">
+       <div className="pictureLevel1"></div>
+        <h2>{task[countTask].text}</h2>
+        <p>{infoBox}</p>
+        <p>{feedback}</p>
+        <p>{points}</p>
+        <button onClick={clickInfo}>More Info</button>
+        <button onClick={clickYes}>Yes</button>
+        <button onClick={clickNo}>No</button>
       </div>
     </div>
   );
--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -10,17 +10,21 @@ console.log(user)
 
   const location = useLocation();
   let header = "Welcome to the game!";
+  const [count, setCount] = useState(0);
 
   if (location.pathname === "/game/level1") {
     header = "Level1";
+ 
   } else if (location.pathname === "/game/level2") {
     header = "Level2";
+
   } else if (location.pathname === "/game/level3") {
     header = "Level3";
   }
 
   function goback() {
     let path = props.location;
+    setCount(0)
     navigate(path);
   }
 
